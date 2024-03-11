80ef13641ab1e6e7393b9d7086826b4a946c98ea
rmsxo5678 <rmsxo5678@naver.com>
2024-03-11 09:42:05 +0900

    카카오 로그인 카카오 로그인

--- app/src/main/java/kr/rmsxo/jetpack_shoppingmall/di/App.kt
+++ app/src/main/java/kr/rmsxo/jetpack_shoppingmall/di/App.kt
@@ -1,8 +1,16 @@
 package kr.rmsxo.jetpack_shoppingmall.di
 
 import android.app.Application
+import com.kakao.sdk.common.KakaoSdk
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class App: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        KakaoSdk.init(this,"NATIVE_APP_KEY")
+    }
+
 }
